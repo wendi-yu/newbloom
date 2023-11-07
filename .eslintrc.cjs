4525608d2a7c58288f42ebcb02664e00fbda7c1e
Wendi Yu <wendi.yu1@gmail.com>
2023-11-07 12:33:08 -0500

    Fix lints

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,8 +7,19 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
+  parser: '@babel/eslint-parser',
   ignorePatterns: ['dist', '.eslintrc.cjs', '.github/*'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    }
+  },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
--- src/pages/Document.jsx
+++ src/pages/Document.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function Document() {
   return (
     <div className='document'>
