995c3022c152a6d64e3aa569edf0af2494048a58
Emily <s265lin@uwaterloo.ca>
2023-11-29 16:13:14 -0500

    redaction popover

---
+++ src/components/document/Redaction/RedactionPopover.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button, Popover } from "antd";
+import CheckIcon from "@/assets/check_fill.svg";
+import CloseIcon from "@/assets/close_fill.svg";
+import CommentIcon from "@/assets/comment_fill.svg";
+
+export default function RedactionPopover() {
+
+    const [open, setOpen] = useState(false);
+
+    const hide = () => {
+        setOpen(false);
+    };
+
+    const content = (
+        <div>
+            <img src={CheckIcon} onClick={hide}/>
+            <img src={CloseIcon} onClick={hide}/>
+            <img src={CommentIcon} onClick={hide}/>
+        </div>
+    );
+
+    const handleOpenChange = (newOpen) => {
+        setOpen(newOpen);
+    };
+
+    return (
+        <div style={{ marginTop: 100, marginLeft: 100 }}>
+            <Popover
+                content={content}
+                trigger="click"
+                open={open}
+                onOpenChange={handleOpenChange}
+                placement="topRight"
+                arrow={false}
+            >
+                <Button type="primary">Click me</Button>
+            </Popover>
+        </div>
+    );
+
+}
