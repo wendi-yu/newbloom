39af21270e0670cf421896416c1dbb5ff49ee130
Wendi Yu <wendi.yu1@gmail.com>
2023-11-29 14:18:22 -0500

    Change example document text to real text

--- src/assets/example_document.js
+++ src/assets/example_document.js
@@ -9,27 +9,30 @@ const ExampleDocument = {
             type: "paragraph",
             children: [
                 {
-                    text: "Text 1",
+                    text: "Seedlings Life Science Ventures, LLC [Seedlings]",
                     [getMarkForCommentThreadID(comments[1].id)]: true,
                 },
                 {
-                    text: "Text 2",
+                    text: " is in the business of early-stage health-care related research and product development. It alleges that Pfizer Canada ULC [Pfizer]",
                     [getMarkForCommentThreadID(overlappingCommentThreadID)]: true,
                 },
                 {
-                    text: "Text 3",
+                    text: ", a major pharmaceutical company, infringes its patent by selling in Canada an auto-injector commonly known as the EpiPen. While, at first sight, the EpiPen and Seedlings’s invention do",
                     [getMarkForCommentThreadID(overlappingCommentThreadID)]: true,
                     [getMarkForCommentThreadID(comments[2].id)]: true,
                 },
                 {
                     text: "Text 4",
                     [getMarkForCommentThreadID(comments[3].id)]: true,
+                }, {
+                    text: " not look alike, Seedlings argues that the EpiPen infringes certain claims of its patent and seeks compensation and an accounting of profits.",
+                    [getMarkForCommentThreadID(comments[3].id)]: true,
                 },
                 {
                     text:
-                        " in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                        " Pfizer denies that the EpiPen infringes upon Seedlings’s patent. Moreover, by way of counterclaim, it seeks a declaration that the claims of Seedlings’s patent asserted in this action are invalid.",
                 },
-                { text: " massa." },
+                { text: " Pfizer argues that those claims are overly broad, obvious and anticipated by prior art.  It also argues that Seedlings has never demonstrated the utility of its invention." },
             ],
         },
         {
@@ -38,11 +41,11 @@ const ExampleDocument = {
                 {
                     [getMarkForCommentThreadID(comments[4].id)]: true,
                     text:
-                        "Cras maximus auctor congue. Sed ultrices elit quis tortor ornare, non gravida turpis feugiat. Morbi facilisis sodales sem quis feugiat. Vestibulum non urna lobortis, semper metus in, condimentum ex. Quisque est justo, egestas sit amet sem ac, auctor ultricies lacus. Pellentesque lorem justo, rhoncus ut magna sit amet, rhoncus posuere libero.",
+                        "I agree with Pfizer that the claims asserted by Seedlings",
                 },
                 {
                     text:
-                        "Cras maximus auctor congue. Sed ultrices elit quis tortor ornare, non gravida turpis feugiat. Morbi facilisis sodales sem quis feugiat. Vestibulum non urna lobortis, semper metus in, condimentum ex. Quisque est justo, egestas sit amet sem ac, auctor ultricies lacus. Pellentesque lorem justo, rhoncus ut magna sit amet, rhoncus posuere libero.",
+                        " are invalid, because they are all overly broad, some of them are anticipated and one of them is obvious. Moreover, had those claims been valid, I would have found that they are not infringed by the EpiPen. In so finding, I am not denying the creative value of Seedlings’s work. Indeed, I am invalidating only a subset of the claims of Seedlings’s patent. Contrary to Seedlings’s assertion, however, this is not a case of two inventors making the same invention independently, with Seedlings being the first in time to file its patent application.",
                 },
             ],
         },
@@ -52,11 +55,11 @@ const ExampleDocument = {
                 {
                     [getMarkForCommentThreadID(comments[5].id)]: true,
                     text:
-                        "Cras maximus auctor congue. Sed ultrices elit quis tortor ornare, non gravida turpis feugiat. Morbi facilisis sodales sem quis feugiat. Vestibulum non urna lobortis, semper metus in, condimentum ex. Quisque est justo, egestas sit amet sem ac, auctor ultricies lacus. Pellentesque lorem justo, rhoncus ut magna sit amet, rhoncus posuere libero.",
+                        "Rather, Seedlings’s auto-injector and the EpiPen are different inventions. The creative use of language in Seedlings’s patent cannot obscure this reality.",
                 },
                 {
                     text:
-                        "Cras maximus auctor congue. Sed ultrices elit quis tortor ornare, non gravida turpis feugiat. Morbi facilisis sodales sem quis feugiat. Vestibulum non urna lobortis, semper metus in, condimentum ex. Quisque est justo, egestas sit amet sem ac, auctor ultricies lacus. Pellentesque lorem justo, rhoncus ut magna sit amet, rhoncus posuere libero.",
+                        " and the EpiPen are different inventions. The creative use of language in Seedlings’s patent cannot obscure this reality. Part I of this judgment describes the auto-injectors involved in this case. In Part II, I identify the skilled person to whom the patent is directed and I give my interpretation of certain terms of the patent, the meaning of which is in dispute. Part III is devoted to the analysis of Pfizer’s challenge to the validity of the patent’s relevant claims. I address the issues of anticipation, obviousness, utility, overbreadth and insufficiency. Even though I conclude that the relevant claims are invalid, I assess, in Part IV, whether the current version of the EpiPen infringes Seedlings’s patent. While I conclude that it does not, I also give my opinion, in Part V, as to the compensation that Pfizer would have owed to Seedlings if it had infringed valid claims.",
                 },
             ],
         },
