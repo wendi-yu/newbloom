781ba9d2c17423f26a3405de90d7dee9d8104b9b
Emily <s265lin@uwaterloo.ca>
2023-11-30 09:56:40 -0500

    redaction popover styling fixed

--- src/components/document/Redaction/RedactionPopover.jsx
+++ src/components/document/Redaction/RedactionPopover.jsx
@@ -1,8 +1,9 @@
 import {useState, React} from "react";
-import { Button, Popover } from "antd";
+import { Button, Popover, ConfigProvider } from "antd";
 import CheckIcon from "@/assets/check_fill.svg";
 import CloseIcon from "@/assets/close_fill.svg";
 import CommentIcon from "@/assets/comment_fill.svg";
+import { lineHeight } from "@mui/system";
 
 export default function RedactionPopover() {
 
@@ -14,9 +15,9 @@ export default function RedactionPopover() {
 
     const content = (
         <div className="flex flex-row">
-            <img src={CheckIcon} onClick={hide}/>
-            <img src={CloseIcon} onClick={hide}/>
-            <img src={CommentIcon} onClick={hide}/>
+            <img src={CheckIcon} onClick={hide} className="object-contain h-5 w-5"/>
+            <img src={CloseIcon} onClick={hide}  className="object-contain h-5 w-5"/>
+            <img src={CommentIcon} onClick={hide}  className="object-contain h-5 w-5"/>
         </div>
     );
 
@@ -26,17 +27,27 @@ export default function RedactionPopover() {
 
     return (
         <div style={{ marginTop: 100, marginLeft: 100 }}>
-            <Popover
-                content={content}
-                trigger="click"
-                open={open}
-                onOpenChange={handleOpenChange}
-                placement="topRight"
-                arrow={false}
-                style={{padding:0, paddingSM:0}}
+        <ConfigProvider
+            theme={{
+                token: {
+                    padding: 0,
+                    paddingSM: 0,
+                    boxShadowSecondary: ""
+                },
+            }}
             >
-                <Button type="primary">Click me</Button>
-            </Popover>
+                <Popover
+                    content={content}
+                    trigger="click"
+                    open={open}
+                    onOpenChange={handleOpenChange}
+                    placement="topRight"
+                    arrow={false}
+                    style={{padding:0, paddingSM:0}}
+                >
+                    <Button type="primary">Click me</Button>
+                </Popover>
+            </ConfigProvider>
         </div>
     );
 
