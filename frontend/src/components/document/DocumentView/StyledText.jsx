e035372bf87660fc8e8a55b7f62765cfce5d493f
Emily <s265lin@uwaterloo.ca>
2024-02-16 17:22:04 -0500

    add redaction popover

--- frontend/src/components/document/DocumentView/StyledText.jsx
+++ frontend/src/components/document/DocumentView/StyledText.jsx
@@ -1,9 +1,31 @@
 import { getCommentThreadsOnTextNode } from "@/util/editorCommentUtils";
 import CommentedText from "./CommentedText";
+import RedactionPopover from "../Redactions/RedactionPopover";
+import {useState, useEffect, React} from "react";
 
 export default function StyledText({ attributes, children, leaf }) {
 
-  //TO DO: trim space off of highlight
+  const [redactionStatus, setRedactionStatus] = useState('');
+
+  useEffect(() => {
+    if (leaf.suggested) {
+      setRedactionStatus('bg-curr-redaction');
+    }
+  }, [leaf.suggested]);
+
+  if (leaf.suggested) {
+    children = (
+      <RedactionPopover
+        text={<span className={redactionStatus}>{children}</span>}
+        onAccept={() => {
+          setRedactionStatus('bg-accepted-redaction');
+        }}
+        onReject={() => {
+          setRedactionStatus('bg-rejected-redaction underline');
+        }}
+      />  
+    );
+  }
 
   if (leaf.current) {
     children = <span className="bg-curr-redaction">{children}</span>
