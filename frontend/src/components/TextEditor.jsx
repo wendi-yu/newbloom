acc820012bdc2ebbdf0031ec78084af066b30cce
Emily <s265lin@uwaterloo.ca>
2024-02-16 18:04:25 -0500

    remove focus border

--- frontend/src/components/TextEditor.jsx
+++ frontend/src/components/TextEditor.jsx
@@ -25,9 +25,9 @@ export default function TextEditor({ document = [], onChange }) {
   return (
     <div className={"flex flex-col"}>
       <div className="bg-document-background flex flex-row justify-center">
-        <div className={"bg-white mx-40 my-20 p-16 max-w-4xl min-h-screen"}>
+        <div className={"bg-white mx-40 my-20 p-16 max-w-4xl min-h-screen "}>
           <Slate editor={editor} initialValue={document} onChange={onChange} placeholder="FUCK">
-            <Editable renderElement={renderElement} renderLeaf={renderLeaf} className="flex flex-col" />
+            <Editable renderElement={renderElement} renderLeaf={renderLeaf} className="flex flex-col focus:outline-none" />
           </Slate>
         </div>
       </div>
