3ecdc4149a4db0546a43f3266ddfd029553f51c6
Emily <s265lin@uwaterloo.ca>
2024-03-05 19:21:28 -0500

    simplified remove redaction

--- frontend/src/hooks/useAddRedactionToState.jsx
+++ frontend/src/hooks/useAddRedactionToState.jsx
@@ -5,9 +5,8 @@ import {
   
   import { useRecoilCallback } from "recoil";
   
-  //call this to add a new redaction
+ //adds redaction to state, 
   export default function useAddRedactionToState() {
-    console.log(redactionIDsState)
     return useRecoilCallback(
       ({ set }) => (id, threadData) => {
         set(redactionIDsState, (ids) => new Set([...Array.from(ids), id]));
@@ -20,20 +19,11 @@ import {
   export default function useRemoveRedactionFromState(mark) {
     return useRecoilCallback(
       ({ set }) => (id) => {
-        console.log('Removing redaction with ID:', id);
-        set(redactionIDsState, (ids) => {
-          console.log('Current redaction IDs:', ids);
-          const filteredIds = Array.from(ids).filter(item => item !== id);
-          console.log('New redaction IDs:', filteredIds);
-          return new Set(filteredIds);
-        });
-  
-        if (id) {
-          console.log('Setting redaction with ID to undefined:', id);
-          set(redactionsState(id), undefined);
-        }
+        set(redactionIDsState, (ids) => new Set([...ids].filter(item => item !== id)));
+        id && set(redactionsState(id), undefined);
       },
       []
+      
     );
   }
   
