1169c75dd4af36e2cbd434dc35415808dc12f830
Emily <s265lin@uwaterloo.ca>
2024-03-17 09:31:03 -0400

    added redirect to homepage from document view

--- frontend/src/components/document/Header/Header.jsx
+++ frontend/src/components/document/Header/Header.jsx
@@ -2,10 +2,18 @@ import LogoIcon from "@/assets/logo.svg";
 // import ShareIcon from "@/assets/share.svg";
 // import CommentIcon from "@/assets/comment.svg";
 import UserProfile from "@/components/document/Header/UserProfile";
+import { useNavigate } from 'react-router-dom';
 
 export default function Header({ documentName }) {
+
+    const navigate = useNavigate();
+
+    const handleLogoClick = () => {
+        navigate('/home');
+    }
+
     return <div className="flex h-24 sticky items-center justify-between p-7">
-        <div className="flex space-x-4 items-center">
+        <div className="flex space-x-4 items-center" onClick={handleLogoClick}>
             <img src={LogoIcon} alt="NewBloom Logo" className="h-10"/>
             <h2>{documentName}</h2>
         </div>
