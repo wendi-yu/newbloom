6459c53e5d8b1079882239b2bd32379d6009ff5e
emily <44381949+emilyylin@users.noreply.github.com>
2024-03-17 23:27:37 -0400

    Hover Buttons (#48)

--- frontend/src/components/document/Header/ViewToggleButton.jsx
+++ frontend/src/components/document/Header/ViewToggleButton.jsx
@@ -1,6 +1,6 @@
 const ViewToggleButton = ({leadingIcon, trailingIcon, onClick, children, selected, ...props}) => {
     return <button 
-        className={`h-10 w-fit ${selected ? "bg-background-primary" : "bg-gray-100"} flex space-x-1.5 items-center rounded-2xl hover:border-primary-light`}
+        className={`h-10 w-fit ${selected ? "bg-background-primary hover:bg-suggested-redaction" : "bg-gray-100 hover:bg-gray-200"} flex space-x-1.5 items-center rounded-2xl hover:border-none border-none`}
         onClick={onClick}
         {...props}
     >
