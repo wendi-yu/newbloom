2f0c31e298c8573edcc52871242f82f357b0f988
Wendi Yu <wendi.yu1@gmail.com>
2024-03-17 22:55:29 -0400

    Move file id creation to backend

--- frontend/src/components/homepage/UploadButton.jsx
+++ frontend/src/components/homepage/UploadButton.jsx
@@ -21,7 +21,6 @@ const UploadButton = (props) => {
 
   const processFiles = (e) => {
     const files = Array.from(e.target.files);
-    console.log("uploading");
 
     files.forEach((file) => {
       const reader = new FileReader();
