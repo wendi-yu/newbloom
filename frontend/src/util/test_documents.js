5bb21d2701a372e5613e20594faaa243e2b0befe
Wendi Yu <wendi.yu1@gmail.com>
2024-03-18 00:31:22 -0400

    Read files from localstore

--- frontend/src/util/test_documents.js
+++ frontend/src/util/test_documents.js
@@ -1,15 +1,15 @@
 import ExampleDocument from "@/assets/example_document";
 import { randomDate } from "./util_functions";
 
-
 const AllDocs = [...Array(15).keys()].map((i) => {
-    return {
-        id: i,
-        name: 'doc ' + i,
-        dateLastModified: randomDate(new Date(2012, 0, 1), new Date()),
-        owner: i,
-        documentBody: ExampleDocument
-    }
-})
+  return {
+    id: i,
+    name: "FAKE doc " + i,
+    dateLastModified: randomDate(new Date(2012, 0, 1), new Date()),
+    dateAdded: randomDate(new Date(2012, 0, 1), new Date()),
+    owner: i,
+    documentBody: ExampleDocument,
+  };
+});
 
-export default AllDocs
+export default AllDocs;
