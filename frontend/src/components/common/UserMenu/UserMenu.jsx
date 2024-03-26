d19921d97b6e7ba00354c96ca96b279e3910735e
Wendi Yu <wendi.yu1@gmail.com>
2024-03-26 14:10:27 -0400

    Add keybinding modal, save to localstore, tie to hotkeys function

--- frontend/src/components/common/UserMenu/UserMenu.jsx
+++ frontend/src/components/common/UserMenu/UserMenu.jsx
@@ -1,37 +1,40 @@
-import SignoutSVG from "@/assets/signout.svg"
-import CustomizeSVG from "@/assets/customize_bindings.svg"
-import SettingsSVG from "@/assets/settings.svg"
+import SignoutSVG from "@/assets/signout.svg";
+import CustomizeSVG from "@/assets/customize_bindings.svg";
+import SettingsSVG from "@/assets/settings.svg";
 
-import UserMenuItem from "@/components/common/UserMenu/UserMenuItem"
-import { useNavigate } from 'react-router-dom';
+import UserMenuItem from "@/components/common/UserMenu/UserMenuItem";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { KeyBindMenu } from "./KeyBindMenu";
 
 //add width prop
-function UserMenu () {
+function UserMenu() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const signout = () => {
+    navigate("/login");
+  };
 
-    const signout = () => {
-        navigate('/login')
-    }
+  const [keyModalOpen, setKeyModalOpen] = useState(false);
 
-    return (
-        <div className="flex flex-col ">
-            <UserMenuItem
-                icon={<img src={SettingsSVG}/>}
-                text="Settings"
-            />
-            <UserMenuItem
-                icon={<img src={CustomizeSVG}/>}
-                text="Customize Bindings"
-            />
-            <UserMenuItem
-                icon={<img src={SignoutSVG}/>}
-                text="Sign Out"
-                onClick={signout}
-            />
-        </div>
-    );
-
+  return (
+    <div>
+      <div className="flex flex-col ">
+        <UserMenuItem icon={<img src={SettingsSVG} />} text="Settings" />
+        <UserMenuItem
+          icon={<img src={CustomizeSVG} />}
+          text="Customize Bindings"
+          onClick={() => setKeyModalOpen(true)}
+        />
+        <UserMenuItem
+          icon={<img src={SignoutSVG} />}
+          text="Sign Out"
+          onClick={signout}
+        />
+      </div>
+      <KeyBindMenu open={keyModalOpen} setOpen={setKeyModalOpen} />
+    </div>
+  );
 }
 
-export default UserMenu
+export default UserMenu;
